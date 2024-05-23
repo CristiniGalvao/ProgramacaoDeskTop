c45dc102cedefd8732479b6c02a35a728b39e3df
Gabriella Cristini <126834537+CristiniGalvao@users.noreply.github.com>
2024-05-22 21:52:07 -0300

    add

---
+++ UniparDigital/src/main/java/service/EnderecoService.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package service;
+
+/**
+ *
+ * @author aluno
+ */
+public class EnderecoService {
+    
+    private static String URL = "http://viacep.com.br/ws/";
+    private static int SUCESSO = 200;
+}
